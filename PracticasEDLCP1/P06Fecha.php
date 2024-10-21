d9abe8ef563ff1954b1bb65baa626d7059d8804f
ErickDLCP <erick10messi2020@gmail.com>
2024-10-21 10:47:13 -0600

    Delete PracticasEDLCP1 directory

--- PracticasEDLCP1/P06Fecha.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dia=date("d");
-
-if($dia<=11)
-	{echo"sitio activo";}
-else
-	{echo"sitio fuera de servicio";}
-?>
--- PracticasEDLCP1/P09Diasemana.php
+++
@@ -1,6 +0,0 @@
-<?php
-$Dia;
-date("D");
-$Dia = date("D");
-echo "Hoy es ",$Dia ;
-?>
